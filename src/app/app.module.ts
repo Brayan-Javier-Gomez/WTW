7a32b9852cbdd33cbbe5026e8a8d23341d177cef
Brayan Javier <brayanjaviergomez.g@gmail.com>
2023-01-16 18:21:31 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './components/personas/personas.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { LoginComponent } from './components/login/login.component';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from "@angular/common/http";
+import { HeaderComponent } from './components/shared/header/header.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent,
+    UsuariosComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
